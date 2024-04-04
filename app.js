7c36324fbfe570d292c2edce14f2af465f50ad72
Nami <hainam.dinhnguyen@gmail.com>
2024-04-04 13:50:53 +0200

    Updated Shemas

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const dataRouter = require('./routes/data')
 
-var app = express();
+const app = express();
+
+//set-up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb+srv://Nami:Aquilateam123@cluster01.ntzuwtd.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster01";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/data', dataRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ database/mongoose.js
@@ -0,0 +1,3 @@
+//MongoDB server string
+
+mongodb+srv://Nami:Aquilateam123@cluster01.ntzuwtd.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster01
